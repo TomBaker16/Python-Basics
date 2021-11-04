ee3e381c52983f2dd9c2864f97a710ed445639ca
TomBaker16 <36339658+TomBaker16@users.noreply.github.com>
2021-11-04 22:11:45 +0000

    Update tictactoe_compiled.py
    updated with some more comments

--- tictactoe_compiled.py
+++ tictactoe_compiled.py
@@ -1,21 +1,21 @@
 import itertools
 
 
-def win(y):
+def win(y): #defines the function to determine if a win has occured
 
-    def all_same(l):
+    def all_same(l):  #function within the win function that checks for the win
         if l.count(l[0]) == len(l) and l[0] != 0: 
             return True
-        else:
-            return False
-
-    #Horizontal check
+        else:                 #the True/False logic here relates to the "all_same" function. If the 'if' statement is correct
+            return False    #then True is returned and the function execute and passes the code. If not then it returns false
+                            #and the code isn't passed
+    #Horizontal look
     for row in game:
-        if all_same(row):  
-            print(f"Player {row[0]} is the winner horizontally! (---)")
-            return True
+        if all_same(row):  #apply the "all_same" function and use row as the parameter. if the function returns True, the next
+            print(f"Player {row[0]} is the winner horizontally! (---)")    #code is executed.
+            return True   #this makes the win function True, a win has happened
 
-    #Diagonal check
+    #Diagonal look
     diags_u = []  
     for col, row in enumerate(reversed(range(len(game)))):
         diags_u.append(game[row][col])
@@ -31,7 +31,7 @@ def win(y):
         return True
 
 
-    #vertical check
+    #vertical look
     for col in range(len(game)):
         x = []  
         for row in game:   
@@ -39,38 +39,47 @@ def win(y):
         if all_same(x):
             print(f"Player {x[0]} is the winner vertically! (|)")
             return True
-    return False 
+    return False #win function is False if none of the above for loops returns a true
 
 
 def game_board(game_map, choice=0, row=0, column=0, just_display=False): #the function that controls game inputs
+           #the game_map parameter is where the function will display the board
+           #choice parameter is for what object you want to place on the board (eg a 1 or 2)
+           #row and column parameters are for where choice will be placed on the board
+           #just display parameter is to print out the game board
     try:
-        if game_map[row][column] != 0:  #if the location on the game map does not equal 0 
-            print("no can do, choose another")
-            return game_map, False
-        print("   "+"  ".join([str(i) for i in range(len(game_map))]))
-        if not just_display:
-            game_map[row][column] = choice 
-        for i, row in enumerate (game_map): 
-            print (i, row)
-        return game_map, True
+        if game_map[row][column] != 0:  #if the location on the game map does not equal 0, then: 
+            print("no can do, choose another") #(row and column parameters are defined in the function)
+            return game_map, False  #by returning game_map and false, the loop runs again until the location chosen DOES  
+                                    #equal zero, then this loop isn't run and it goes onto the next loop
+        print("   "+"  ".join([str(i) for i in range(len(game_map))]))   #this line is to print the column labels
+                                                                        
+        if not just_display:               #this loop says if just_display=False (the default) then execute the code below.                               
+            game_map[row][column] = choice  #statement says that game_map location at indexes row and column  
+                                                #equal whatever 'choice' is. Puts a 1 or 2 at the location.
+        for i, row in enumerate (game_map): #enumerate gives the index followed by the item for the list "game_map"
+            print (i, row)   #this allows the rows of the game to be labelled alongside the game board
+        
+        return game_map, True  #this return command is outside of the loops above. So this happens once the above is done 
+                                #and satisfies the try: command
 
     except IndexError as error:
         print ('choose a different number', error)
         return game_map, False
 
-    except Exception as error:
-        print ('whoops', error)
+    except ValueError:
+        print ('whoops')
         return game_map, False
 
-play = True
-players = [1,2]
-while play:
+play = True        #statement. Allows us to be able to end the game later
+players = [1,2]   #defining a variable, players
+while play:      #while play is True do all of the following
     game = [[0, 0, 0], 
-            [0, 0, 0], 
+            [0, 0, 0],     #defines the game board which will later be edited by game_board function
             [0, 0, 0]]
 
-    game_won = False
-    game, _ = game_board(game, just_display = True)
+    game_won = False    #statement. Allows us to have 'game won' and 'game not yet won' scenarios
+    game_board(game, just_display=True) # game, _ = 
     player_choice = itertools.cycle(players)
     while not game_won:
         current_player = next(player_choice)
@@ -80,7 +89,7 @@ while play:
         while not played:
             column_choice = int(input("What column? (0, 1, 2): "))
             row_choice = int(input("What row? (0, 1, 2): "))
-            game, played = game_board(game, current_player, row_choice, column_choice)
+            game, played = game_board(game, current_player, row_choice, column_choice) 
 
         if win(game):
             game_won = True
@@ -93,11 +102,3 @@ while play:
             else:
                 print("not valid answer, bye")
                 play = False
-
-
-
-
-
-
-
-
